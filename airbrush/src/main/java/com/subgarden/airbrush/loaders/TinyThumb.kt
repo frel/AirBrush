6e6e0bc6e48785d09f1ad660ec7d4ba04d9a45d1
Fredrik H. Larsen <f@subgarden.com>
2018-09-02 16:41:00 +0200

    Preparing AirBrush 0.6.1
    - TinyThumbDecoder now supports a different default base64 decode flag.
    - TinyThumb can now override the default base64 decode flag.
    - Removed bitmapProvider from TinyThumbDecoder. A custom decoder class should be used instead.
    - Added @JvmOverload to better support default arguments from Java
    - Added more documentation
    - Upgraded to latest kotlin and support library versions

--- airbrush/src/main/java/com/subgarden/airbrush/loaders/TinyThumb.kt
+++ airbrush/src/main/java/com/subgarden/airbrush/loaders/TinyThumb.kt
@@ -2,11 +2,17 @@ package com.subgarden.airbrush.loaders
 
 
 /**
- * A base64 encoded jpeg. Typically very small.
+ * A base64 encoded JPEG. Typically very small.
  *
+ * @param base64 The base64 encoded JPEG
+ * @param header An optional header that can be used to reduce the size of the base64 payload
+ * @param base64DecodeFlag Optional flag to override the Base64 decode flag set by the TinyThumbDecoder.
  * @author Fredrik Larsen (fredrik@subgarden.com)
  */
-data class TinyThumb(val base64: String, val header: String = "") {
+data class TinyThumb @JvmOverloads constructor(
+        val base64: String,
+        val header: String = "",
+        val base64DecodeFlag: Int = -1) {
     init {
         require(base64.isNotEmpty()) { "Missing data. Base64 string is empty." }
     }
